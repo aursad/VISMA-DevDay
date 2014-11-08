965b80c9100c6a12e3a4bd1a0b0280ea7382b642
Aurimas Sadauskas <aurimas.sadauskas@visma.com>
2014-11-08 10:41:27 +0200

    Init project files

---
+++ DevDay/DevDay/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace DevDay
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
